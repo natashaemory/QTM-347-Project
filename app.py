d20f519fad8a95e80c11150f73b75267ad1b0b36
Natasha Gandhi <natashagandhi02@gmail.com>
2024-04-24 22:06:08 -0400

    code

---
+++ app.py
@@ -0,0 +1,9 @@
+from sklearn.tree import DecisionTreeRegressor as DTR
+from matplotlib.pyplot import subplots
+from sklearn.tree import plot_tree
+import sklearn.model_selection as skm
+from ISLP.models import ModelSpec as MS
+from sklearn.tree import export_text
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
